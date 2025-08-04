d55b502223d1da00bf3d049c735c776a646ab594
EJ <estanislaojose.suarez@eclectustech.net>
2025-08-05 00:29:04 +0800

    removed web socket

--- src/shared/services/index.js
+++ src/shared/services/index.js
@@ -3,7 +3,6 @@ export * from "./loading-services.js";
 export * from "./user-services.js";
 export * from "./modal.services.js";
 export * from "./date-services.js";
-export * from "./messages-services.js";
 export * from "./order-services.js";
 export * from "./item-type-services.js";
 export * from "./item-services.js";
